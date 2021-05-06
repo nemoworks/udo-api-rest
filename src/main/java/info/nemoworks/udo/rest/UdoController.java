b018c4cffdc5647defd024d1eec9b53d5c2f7ec8
Chun <caochun@gmail.com>
2021-05-06 21:14:48 +0800

    udo integration demonstration

--- src/main/java/info/nemoworks/udo/rest/UdoController.java
+++ src/main/java/info/nemoworks/udo/rest/UdoController.java
@@ -1,13 +1,21 @@
 package info.nemoworks.udo.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import info.nemoworks.udo.service.UdoService;
+
 @Controller
 public class UdoController {
-    @GetMapping(value="/")
-    public Udo welcome(){
-        return new Udo();
+
+    // @Autowired
+    // private UdoService udoService;
+
+    @GetMapping(value ="/")
+    public void welcome(){
+
+        // udoService.notify();
     }
     
 }
--- src/main/java/info/nemoworks/udo/rest/UdoRestApplication.java
+++ src/main/java/info/nemoworks/udo/rest/UdoRestApplication.java
@@ -1,13 +1,44 @@
 package info.nemoworks.udo.rest;
 
+import java.util.Arrays;
+
+import com.google.common.eventbus.EventBus;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "info.nemoworks.udo")
+@Slf4j
 public class UdoRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UdoRestApplication.class, args);
+	}
+
+	@Bean
+	public EventBus eventBus() {
+		return new EventBus();
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			log.info("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				log.info(beanName);
+			}
+		};
 	}
 
 }
