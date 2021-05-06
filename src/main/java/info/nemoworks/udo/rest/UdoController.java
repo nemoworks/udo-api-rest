099e37a13201222c2e670559748e824b9b81605a
Chun <caochun@gmail.com>
2021-05-06 18:48:26 +0800

    init

---
+++ src/main/java/info/nemoworks/udo/rest/UdoController.java
@@ -0,0 +1,13 @@
+package info.nemoworks.udo.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UdoController {
+    @GetMapping(value="/")
+    public Udo welcome(){
+        return new Udo();
+    }
+    
+}
---
+++ src/main/java/info/nemoworks/udo/rest/UdoRestApplication.java
@@ -0,0 +1,13 @@
+package info.nemoworks.udo.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdoRestApplication.class, args);
+	}
+
+}
