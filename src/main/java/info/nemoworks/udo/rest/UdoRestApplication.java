76e4406982b980497523efebd7109aa7b0f62eb7
NovelistChan <774527381@qq.com>
2021-05-25 17:49:02 +0800

    add create by uri

--- src/main/java/info/nemoworks/udo/rest/UdoRestApplication.java
+++ src/main/java/info/nemoworks/udo/rest/UdoRestApplication.java
@@ -1,55 +1,54 @@
 package info.nemoworks.udo.rest;
 
-import java.util.Arrays;
-
 import com.google.common.eventbus.EventBus;
-
 import info.nemoworks.udo.messaging.HTTPServiceGateway;
+import info.nemoworks.udo.service.SaveByUriEventHandler;
 import info.nemoworks.udo.service.SyncEventHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "info.nemoworks.udo")
 @Slf4j
-public class UdoRestApplication implements CommandLineRunner{
+public class UdoRestApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UdoRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UdoRestApplication.class, args);
+    }
 
 
-	@Autowired
+    @Autowired
     private HTTPServiceGateway httpServiceGateway;
 
-	@Autowired
+    @Autowired
     EventBus eventBus;
 
-	@Autowired
+    @Autowired
     SyncEventHandler syncEventHandler;
 
-	@PostConstruct
-    public void registerEventHandler(){
+    @Autowired
+    SaveByUriEventHandler saveByUriEventHandler;
+
+    @PostConstruct
+    public void registerEventHandler() {
         eventBus.register(httpServiceGateway);
         eventBus.register(syncEventHandler);
+        eventBus.register(saveByUriEventHandler);
     }
 
 
     @Override
     public void run(String... args) throws Exception {
 
-        while(true){
+        while (true) {
             System.out.println("start...");
-            if(httpServiceGateway.getEndpoints().size()>0){
+            if (httpServiceGateway.getEndpoints().size() > 0) {
                 httpServiceGateway.start();
             }
             try {
