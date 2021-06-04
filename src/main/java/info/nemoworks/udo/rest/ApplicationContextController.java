ea8734a46cca057813fe09cb189ce896438a0fc8
TcPaulyue <1195882464@qq.com>
2021-06-04 13:37:07 +0800

    add appCtx api

--- src/main/java/info/nemoworks/udo/rest/ApplicationContextController.java
+++ src/main/java/info/nemoworks/udo/rest/ApplicationContextController.java
@@ -1,12 +1,16 @@
 package info.nemoworks.udo.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.eventbus.EventBus;
+import com.google.gson.JsonObject;
 import info.nemoworks.udo.messaging.gateway.HTTPServiceGateway;
 import info.nemoworks.udo.messaging.messaging.ApplicationContext;
 import info.nemoworks.udo.messaging.messaging.ApplicationContextCluster;
 import info.nemoworks.udo.messaging.messaging.Publisher;
 import info.nemoworks.udo.messaging.messaging.Subscriber;
 import info.nemoworks.udo.model.Udo;
+import info.nemoworks.udo.model.UdoType;
+import info.nemoworks.udo.service.UdoService;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +25,9 @@ public class ApplicationContextController {
     private final Publisher publisher;
 
     private final Subscriber subscriber;
+
+    @Autowired
+    UdoService udoService;
 
     @Autowired
     EventBus eventBus;
@@ -42,6 +49,16 @@ public class ApplicationContextController {
         udo.setId("udo");
         applicationContext.subscribeMessage(applicationContext.getAppId(),udo);
         return applicationContext.getAppId();
+    }
+
+    @PostMapping("/applicationContext/{applicationContextId}")
+    public void addUdoInApplicationContext(@PathVariable String applicationContextId,@RequestParam String udoId) throws JsonProcessingException, MqttException {
+        Udo udo = udoService.getUdoById(udoId);
+        ApplicationContext applicationContext = ApplicationContextCluster.getApplicationContextMap()
+                .get(applicationContextId).getValue0();
+        ApplicationContextCluster.getApplicationContextMap().get(applicationContextId)
+                .getValue1().add(udo.getId());
+        applicationContext.subscribeMessage(applicationContext.getAppId(),udo);
     }
 
     @DeleteMapping("/applicationContext")
